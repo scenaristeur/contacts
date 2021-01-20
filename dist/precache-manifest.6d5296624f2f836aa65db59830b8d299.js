50b6001cdceed465321d6aed8cbf07bf1a56d297
Scenaristeur <scenaristeur@gmail.com>
2021-01-20 12:54:34 +0100

    test axios semapps post

---
+++ dist/precache-manifest.6d5296624f2f836aa65db59830b8d299.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "203d613f5329ca9b4b59",
+    "url": "/ldp-workspace/css/app.4478ea10.css"
+  },
+  {
+    "revision": "5ca7789086acb180d249",
+    "url": "/ldp-workspace/css/chunk-59d5d60e.2a9fb7c3.css"
+  },
+  {
+    "revision": "9ca4f2a9ae20c956cc83",
+    "url": "/ldp-workspace/css/chunk-vendors.378af094.css"
+  },
+  {
+    "revision": "cf945d583574f68b2d14e6dda669acb7",
+    "url": "/ldp-workspace/index.html"
+  },
+  {
+    "revision": "da1d596d80cca7ec6159",
+    "url": "/ldp-workspace/js/about.205580bf.js"
+  },
+  {
+    "revision": "203d613f5329ca9b4b59",
+    "url": "/ldp-workspace/js/app.33a13f14.js"
+  },
+  {
+    "revision": "8f2ea7b7b953f9375208",
+    "url": "/ldp-workspace/js/chunk-2d0c849f.62067d64.js"
+  },
+  {
+    "revision": "5ca7789086acb180d249",
+    "url": "/ldp-workspace/js/chunk-59d5d60e.4404ca17.js"
+  },
+  {
+    "revision": "d1fe8061db137a1d77f1",
+    "url": "/ldp-workspace/js/chunk-75299b9e.94f781ea.js"
+  },
+  {
+    "revision": "9ca4f2a9ae20c956cc83",
+    "url": "/ldp-workspace/js/chunk-vendors.a47c2258.js"
+  },
+  {
+    "revision": "6c737ebf0dfffa6af090fe1ed6bf7539",
+    "url": "/ldp-workspace/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ldp-workspace/robots.txt"
+  }
+]);
