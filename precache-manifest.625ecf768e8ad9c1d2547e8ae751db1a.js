77524ec254ed78d5f355ab88c5b85af55ca53d9d
Scenaristeur <scenaristeur@gmail.com>
2021-02-01 16:24:19 +0100

    put vcard in pouchdb & save in thing

---
+++ precache-manifest.625ecf768e8ad9c1d2547e8ae751db1a.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3065b6ad3b21bf0b860ccc74d7f2879",
+    "url": "/ldp-workspace/404.html"
+  },
+  {
+    "revision": "fe2e8891ec2949c0759a",
+    "url": "/ldp-workspace/css/app.a2952c0e.css"
+  },
+  {
+    "revision": "917d88a5b311e293e994",
+    "url": "/ldp-workspace/css/chunk-59d5d60e.2a9fb7c3.css"
+  },
+  {
+    "revision": "79aa5abc77368b4f8120",
+    "url": "/ldp-workspace/css/chunk-befc1884.2a9fb7c3.css"
+  },
+  {
+    "revision": "35a3ee82685bf701ab41",
+    "url": "/ldp-workspace/css/chunk-d9ccf18c.decb59b3.css"
+  },
+  {
+    "revision": "f80cbb6339b5b1f6ebbe",
+    "url": "/ldp-workspace/css/chunk-vendors.23e2f014.css"
+  },
+  {
+    "revision": "a4fa21e5b9fb570a9747",
+    "url": "/ldp-workspace/css/contacts.c92ac7d4.css"
+  },
+  {
+    "revision": "4c5b552cf835fedefd97",
+    "url": "/ldp-workspace/css/tests.a66629a4.css"
+  },
+  {
+    "revision": "48712b2a81296b4a28b63357506705db",
+    "url": "/ldp-workspace/index.html"
+  },
+  {
+    "revision": "1e1cdea411b4290a6003",
+    "url": "/ldp-workspace/js/about.9f0a222f.js"
+  },
+  {
+    "revision": "fe2e8891ec2949c0759a",
+    "url": "/ldp-workspace/js/app.735aa31e.js"
+  },
+  {
+    "revision": "84c8ab48e572cce30f382692121bd8c3",
+    "url": "/ldp-workspace/js/browserFS.js"
+  },
+  {
+    "revision": "53abe4ff9e0995b988a4dab830ac52af",
+    "url": "/ldp-workspace/js/browserfs.min.js"
+  },
+  {
+    "revision": "b79817f7bf384a60da4c",
+    "url": "/ldp-workspace/js/chunk-23187bda.c28831ab.js"
+  },
+  {
+    "revision": "44d618d02f602fa2d9ce",
+    "url": "/ldp-workspace/js/chunk-2d0a386e.5f608f65.js"
+  },
+  {
+    "revision": "1bb5b55ea800467f8df2",
+    "url": "/ldp-workspace/js/chunk-2d0b6714.5ba753c8.js"
+  },
+  {
+    "revision": "1d26af658ad4ade73bbc",
+    "url": "/ldp-workspace/js/chunk-2d21659a.df6b8bb4.js"
+  },
+  {
+    "revision": "22871bc996ef89245ee2",
+    "url": "/ldp-workspace/js/chunk-2d226c5a.1ed47176.js"
+  },
+  {
+    "revision": "917d88a5b311e293e994",
+    "url": "/ldp-workspace/js/chunk-59d5d60e.f49a21d5.js"
+  },
+  {
+    "revision": "79aa5abc77368b4f8120",
+    "url": "/ldp-workspace/js/chunk-befc1884.74564dbd.js"
+  },
+  {
+    "revision": "35a3ee82685bf701ab41",
+    "url": "/ldp-workspace/js/chunk-d9ccf18c.dd67134a.js"
+  },
+  {
+    "revision": "f80cbb6339b5b1f6ebbe",
+    "url": "/ldp-workspace/js/chunk-vendors.df4bd45b.js"
+  },
+  {
+    "revision": "d6765f2b87a2769f53fc",
+    "url": "/ldp-workspace/js/contact.06e0e377.js"
+  },
+  {
+    "revision": "a4fa21e5b9fb570a9747",
+    "url": "/ldp-workspace/js/contacts.a9542f50.js"
+  },
+  {
+    "revision": "ee436bde314b899fc621",
+    "url": "/ldp-workspace/js/editContact.8571dd57.js"
+  },
+  {
+    "revision": "bda1caab0552aefa5399",
+    "url": "/ldp-workspace/js/import.90af1301.js"
+  },
+  {
+    "revision": "8a27e7f1798dbd55be6e",
+    "url": "/ldp-workspace/js/itemvue.c1131c6a.js"
+  },
+  {
+    "revision": "28629856b25e18182ff807d237695bc9",
+    "url": "/ldp-workspace/js/solid-rest.js"
+  },
+  {
+    "revision": "1f674ec90de9b3699df1",
+    "url": "/ldp-workspace/js/solidresttest.12f1045a.js"
+  },
+  {
+    "revision": "4c5b552cf835fedefd97",
+    "url": "/ldp-workspace/js/tests.29c34efa.js"
+  },
+  {
+    "revision": "547b7f506a9c8e6385ae43d21fb87166",
+    "url": "/ldp-workspace/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ldp-workspace/robots.txt"
+  },
+  {
+    "revision": "9ccfd7df5ebb1112798322bbf48dd3fc",
+    "url": "/ldp-workspace/solid-auth-login/popup.html"
+  }
+]);
