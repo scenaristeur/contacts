bfd914b6768c8a52e82be40f2cf005b90a7a55c8
Scenaristeur <scenrister@galcm>
2021-04-10 01:01:24 +0200

    update node_modules

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,7 +3,7 @@
     <b-jumbotron header="Contacts" lead="Contacts store your Contacts on your Solid POD in the rootFolder/contacts/ folder.">
       <p>For more information visit <a href="https://github.com/scenaristeur/ldp-workspace/blob/main/README.md" target="_blank">github repo</a></p>
       <!-- <b-button variant="primary" href="#">More Info</b-button> -->
-      <SolidLoginButton />
+      <!-- <SolidLoginButton /> -->
     </b-jumbotron>
   </div>
 </template>
