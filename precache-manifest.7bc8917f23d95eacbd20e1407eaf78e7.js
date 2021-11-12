09b8d19e179b270493fff7594863ad8604e9e9cd
Scenaristeur <scenaristeur@gmail.com>
2021-11-12 09:28:27 +0100

    migration from ldp-workspace to contacts repo

---
+++ precache-manifest.7bc8917f23d95eacbd20e1407eaf78e7.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09a11624b760149fe93c4f47f30d3a1d",
+    "url": "/contacts/404.html"
+  },
+  {
+    "revision": "dd405cf205d2d57dd34f",
+    "url": "/contacts/css/about.c880b8ce.css"
+  },
+  {
+    "revision": "c31de29cdbdc855745b0",
+    "url": "/contacts/css/app.00a82aa8.css"
+  },
+  {
+    "revision": "435769c14a0097d8df73",
+    "url": "/contacts/css/chunk-20cc4d60.1bbdaac2.css"
+  },
+  {
+    "revision": "4c0f99ca2e21cc1d28c7",
+    "url": "/contacts/css/chunk-2c53a7bc.6f581883.css"
+  },
+  {
+    "revision": "cfba30b8b75a04d3ca0c",
+    "url": "/contacts/css/chunk-2ea70690.4ee9f3c0.css"
+  },
+  {
+    "revision": "4aa5cc95953fe4736293",
+    "url": "/contacts/css/chunk-59d5d60e.4ee9f3c0.css"
+  },
+  {
+    "revision": "28a62c35cc923b459420",
+    "url": "/contacts/css/chunk-80fc5770.4342f808.css"
+  },
+  {
+    "revision": "7eba1f9a07d658cf6101",
+    "url": "/contacts/css/chunk-8f4d58cc.23e3d0e8.css"
+  },
+  {
+    "revision": "f4a59e05729879762085",
+    "url": "/contacts/css/chunk-b79fe53c.0e7e4c50.css"
+  },
+  {
+    "revision": "bd30f19ce8c8b3fc487a",
+    "url": "/contacts/css/chunk-vendors.b1126009.css"
+  },
+  {
+    "revision": "f6a0836f7f26f30e83e2",
+    "url": "/contacts/css/contacts.5dc319d7.css"
+  },
+  {
+    "revision": "7d9a57266db839a739bcf13315a8c83b",
+    "url": "/contacts/dexie.html"
+  },
+  {
+    "revision": "ce1ce81001440b6e8701045562c076f7",
+    "url": "/contacts/index.html"
+  },
+  {
+    "revision": "dd405cf205d2d57dd34f",
+    "url": "/contacts/js/about.bfb25558.js"
+  },
+  {
+    "revision": "c31de29cdbdc855745b0",
+    "url": "/contacts/js/app.8d9e615b.js"
+  },
+  {
+    "revision": "84c8ab48e572cce30f382692121bd8c3",
+    "url": "/contacts/js/browserFS.js"
+  },
+  {
+    "revision": "53abe4ff9e0995b988a4dab830ac52af",
+    "url": "/contacts/js/browserfs.min.js"
+  },
+  {
+    "revision": "435769c14a0097d8df73",
+    "url": "/contacts/js/chunk-20cc4d60.2842d9a1.js"
+  },
+  {
+    "revision": "4c0f99ca2e21cc1d28c7",
+    "url": "/contacts/js/chunk-2c53a7bc.b5ff439d.js"
+  },
+  {
+    "revision": "5a5af4970bec19af4258",
+    "url": "/contacts/js/chunk-2c8fe036.e340eb8c.js"
+  },
+  {
+    "revision": "050b36dc66d991ab9d10",
+    "url": "/contacts/js/chunk-2d0a386e.2781e1bf.js"
+  },
+  {
+    "revision": "15e0a25a34e71bc8205b",
+    "url": "/contacts/js/chunk-2d0b6714.0635bb1a.js"
+  },
+  {
+    "revision": "3e3e62fbf47c32dc8bd7",
+    "url": "/contacts/js/chunk-2d21659a.6ebd7b6f.js"
+  },
+  {
+    "revision": "e00ed6fc5c35ae03187b",
+    "url": "/contacts/js/chunk-2d226c5a.5d88d03f.js"
+  },
+  {
+    "revision": "cfba30b8b75a04d3ca0c",
+    "url": "/contacts/js/chunk-2ea70690.3e711513.js"
+  },
+  {
+    "revision": "4aa5cc95953fe4736293",
+    "url": "/contacts/js/chunk-59d5d60e.1e44e86e.js"
+  },
+  {
+    "revision": "1414364ab25a5bc05383",
+    "url": "/contacts/js/chunk-5a001d9e.3f604c6b.js"
+  },
+  {
+    "revision": "28a62c35cc923b459420",
+    "url": "/contacts/js/chunk-80fc5770.764c73e4.js"
+  },
+  {
+    "revision": "7eba1f9a07d658cf6101",
+    "url": "/contacts/js/chunk-8f4d58cc.4f6998fe.js"
+  },
+  {
+    "revision": "f4a59e05729879762085",
+    "url": "/contacts/js/chunk-b79fe53c.f399e9f8.js"
+  },
+  {
+    "revision": "f2422b86cc9494d7ee3c",
+    "url": "/contacts/js/chunk-e9106412.ec0e2de4.js"
+  },
+  {
+    "revision": "bd30f19ce8c8b3fc487a",
+    "url": "/contacts/js/chunk-vendors.ff4a15ff.js"
+  },
+  {
+    "revision": "0b8c72c612e9faa95d2e",
+    "url": "/contacts/js/contact.bc217d6e.js"
+  },
+  {
+    "revision": "f6a0836f7f26f30e83e2",
+    "url": "/contacts/js/contacts.a050df0d.js"
+  },
+  {
+    "revision": "112264866b95a089e957",
+    "url": "/contacts/js/editContact.101d36d9.js"
+  },
+  {
+    "revision": "f7d8e791dfd7169c731b",
+    "url": "/contacts/js/import.409cae70.js"
+  },
+  {
+    "revision": "4df6d0524af7995b4753",
+    "url": "/contacts/js/itemvue.c82ce0fb.js"
+  },
+  {
+    "revision": "28629856b25e18182ff807d237695bc9",
+    "url": "/contacts/js/solid-rest.js"
+  },
+  {
+    "revision": "422cbd853191a8c9b6a5",
+    "url": "/contacts/js/solidresttest.40e8adfe.js"
+  },
+  {
+    "revision": "ec7e3c97df225089a135",
+    "url": "/contacts/js/synchronisation.99d156dc.js"
+  },
+  {
+    "revision": "8f92f9d0712a92f3022f",
+    "url": "/contacts/js/tests.e8972fd2.js"
+  },
+  {
+    "revision": "69e70eb394aef7e2d4576e404cfab10e",
+    "url": "/contacts/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/contacts/robots.txt"
+  },
+  {
+    "revision": "9ccfd7df5ebb1112798322bbf48dd3fc",
+    "url": "/contacts/solid-auth-login/popup.html"
+  }
+]);
